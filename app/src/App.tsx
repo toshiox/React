319f3fa4b9f744d96bb891c8f9b706ebaca9f647
Gustavo watanabe <gustavo.watanabe@vidibr.com>
2023-10-19 17:27:25 -0300

    fist commit

---
+++ app/src/App.tsx
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/NavBar/index';
+
+function App() {
+  return (
+      <Header/>
+  );
+}
+
+export default App;
