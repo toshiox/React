dad80fa91e7c1edefc12b4092343f3a44b35b7ef
Gustavo watanabe <gustavo.watanabe@vidibr.com>
2023-11-02 20:14:47 -0300

    correcao import plugins

--- app/src/Components/Pages/TextEditor.tsx
+++ app/src/Components/Pages/TextEditor.tsx
@@ -14,11 +14,10 @@ const TextEditor: React.FC = () => {
         init={{
           height: 500, // Altura do editor
           menubar: true, // Barra de menus
-          plugins: [
-            'advlist autolink lists link image charmap print preview anchor',
-            'searchreplace visualblocks code fullscreen',
-            'insertdatetime media table paste code help wordcount',
-          ],
+          plugin_version: '6.6.0',
+          plugins: 
+            'advlist autolink lists link image charmap preview anchor searchreplace visualblocks code fullscreen insertdatetime media table code help wordcount',
+          
           toolbar: 'undo redo | formatselect | ' +
             'bold italic backcolor | alignleft aligncenter ' +
             'alignright alignjustify | bullist numlist outdent indent | ' +
